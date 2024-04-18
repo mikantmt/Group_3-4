d17829ef8d5a513dfb06903ad1b2a116cefb130f
shoya <144639871+relax37@users.noreply.github.com>
2024-04-18 11:11:55 +0900

    score

---
+++ Src/Oda/Score/Score.cpp
@@ -0,0 +1,59 @@
+#include "DxLib.h"
+#include "Score.h"
+
+FILE* fScore;
+
+int Score::ScoreNum;			// スコアの値
+int Score::HighScoreNum;	// ハイスコアの値
+
+// スコアの初期化
+void Score::Init()
+{
+	Score::ScoreNum = 0;
+}
+
+// ハイスコアの初期化
+void Score::Reset()
+{
+	// ハイスコアにスコアを代入
+	Score::HighScoreNum = 0;
+
+	// ファイルを開く(ない場合は自動で新規作成する)
+	errno_t err;
+	err = fopen_s(&fScore, "Data/Score/Score.txt", "w+");
+
+	// ファイルが開けた・または新規作成できた
+	if (fScore != NULL)
+	{
+		// ファイルは閉じる
+		fclose(fScore);
+	}
+}
+
+// スコアの加算
+void Score::Add()
+{
+	Score::ScoreNum += ADD_SCORE_NUM;
+}
+
+// ハイスコアの更新
+void Score::Update()
+{
+	// 保存されたハイスコアより大きかったら
+	if (Score::ScoreNum > Score::HighScoreNum)
+	{
+		// ハイスコアにスコアを代入
+		Score::HighScoreNum = Score::ScoreNum;
+
+		// ファイルを開く(ない場合は自動で新規作成する)
+		errno_t err;
+		err = fopen_s(&fScore, "Data/Score/Score.txt", "w+");
+
+		// ファイルが開けた・または新規作成できた
+		if (fScore != NULL)
+		{
+			// ファイルは閉じる
+			fclose(fScore);
+		}
+	}
+}
---
+++ Src/Oda/Score/Score.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// スコアの加算数
+#define ADD_SCORE_NUM (1)
+
+class Score {
+public:
+	static int ScoreNum;		// スコアの値
+	static int HighScoreNum;	// ハイスコアの値
+
+	// スコアの初期化
+	static void Init();
+
+	// ハイスコアの初期化
+	static void Reset();
+
+	// スコアの加算
+	static void Add();
+
+	// ハイスコアの更新
+	static void Update();
+};
